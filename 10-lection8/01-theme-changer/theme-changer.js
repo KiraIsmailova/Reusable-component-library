ea28dbcb032762b97de739df7f0ef70ebc20ff51
Kira <kira-ismailova@bk.ru>
2023-07-09 23:28:04 +0300

    fix

--- 10-lection8/01-theme-changer/theme-changer.js
+++ 10-lection8/01-theme-changer/theme-changer.js
@@ -4,3 +4,10 @@ const html = document.documentElement;
 changer.addEventListener('change', function() {
     html.toggleAttribute('data-theme-dark');
 });
+
+const change = document.getElementById('theme-change');
+const index = document.documentElement;
+
+change.addEventListener('change', function() {
+    index.toggleAttribute('data-theme-dark');
+});
